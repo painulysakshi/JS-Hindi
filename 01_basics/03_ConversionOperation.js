f71d01f214d8e28e6832d084f01029d83b9e36e3
Sakshi Painuly <124492377+painulysakshi@users.noreply.github.com>
2023-12-30 09:37:29 +0000

    Operations

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -25,7 +25,51 @@
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//*********************Operations**************************/
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);//2 raise to ki power 2
+// console.log(2**3);
+// console.log(2/3); //divide operation
+// console.log(2%3); // to see remainder modulo operator
+
+let str1 = "hello"
+let str2 = " Sakshi"
+// let str3 = str1 + str2
+// console.log(str3);
+
+//Agar string first hai too sabhi ko string mein treat kiya jaiga
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+
+//Agar string last mein hai too pehele jo conversion hai wo ho jainge
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter);
