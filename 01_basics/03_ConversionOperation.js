d327e49ecb357a962ebaf183192d4cc337cda829
Sakshi Painuly <124492377+painulysakshi@users.noreply.github.com>
2023-12-30 08:33:59 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,31 @@
+//let score = "33"
+//let score = "33abc"  //output: number
+//let score = null   // output: 0
+//let score = undefined //output: NaN
+//let score = true  // output: 1
+// let score = "Sakshi" //Output: NaN
+// console.log(typeof score)   
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);  //output: NaN (not a number)
+
+
+//Number Conversion
+/*
+"33"=>33
+"33abc"=>NaN
+"true"=>1; "false=>0"
+*/
+
+//let isLoggedIn = 1 //output: true
+//let isLoggedIn = "" //When convert empty string //output: false
+// let isLoggedIn = "Sakshi" //output: true
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
