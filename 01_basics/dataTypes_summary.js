499d321a400ea7857d502ebc085098a30a0ba56a
Sakshi Painuly <124492377+painulysakshi@users.noreply.github.com>
2023-12-30 14:33:06 +0000

    dataTypeSummary

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,38 @@
+//# Primitive Data Type (call by value hote hai)
+// 7 types: String, Number, Boolean, null, undefined, Symbol (kisi bhi value ko unique banane ke liye symbol ka use krte hai), BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // Or let userEmail = undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); 
+
+// const bigNumber = 34567843225n
+
+
+//#Non Primitive (OR Reference Type) (Basically ye sari values wo hai jo ki memory mein inka Reference directly aapko allocate kiya jaa sakta hai)
+// Arrays, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+// curly brases ke under objects hote hain 
+ let myObj = {
+    name: "Sakshi",
+    age: 23,
+}
+
+const myFunctioin = function(){
+    console.log("Hello World");
+}
+
+// console.log(typeof outsideTemp);
+console.log(typeof myFunctioin);
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3 
