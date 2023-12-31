09a755cc69cd8ced7d8d4e7fc85d1266d635c8c0
Sakshi Painuly <124492377+painulysakshi@users.noreply.github.com>
2023-12-31 14:52:16 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+const name = "Sakshi"
+const count = 50
+
+// console.log(name + count + "value")  [Concatination of strings]
+
+//string interpulation (it is a modern way to write this syntax)
+console.log(`Hello my name is ${name} and my cunt is ${count}`);
+
+const gameName = new String('Sakshi-hc') //2nd String Syntax
+//To access key value pair
+console.log(gameName[0]);
+ //We can access proto type also
+ console.log(gameName.__proto__); 
+
+ //Object use
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(5));
+ console.log(gameName.indexOf('i')); 
+ 
