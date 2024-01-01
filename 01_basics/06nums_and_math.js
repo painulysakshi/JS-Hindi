e6456bd6c4279b05bde552186c8355b49712313a
Sakshi Painuly <124492377+painulysakshi@users.noreply.github.com>
2024-01-01 08:46:50 +0000

    nums_and_math

---
+++ 01_basics/06nums_and_math.js
@@ -0,0 +1,14 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);  //number is change into string
+console.log(balance.toFixed(2)); 
+
+const otherNumber = 123.6645
+console.log(otherNumber.toPrecision(4))
+console.log(otherNumber.toPrecision(3))  //Round off
+
+
